9df634f0f87baffa704ee041fccd4f892fdb4c67
Lucaas_lpp <lepape.lucas@outlook.com>
2025-09-30 17:24:55 +0200

    footer v1 avec fond noir

--- views/home/index.php
+++ views/home/index.php
@@ -11,6 +11,25 @@
     <a class="btn btn-primary" href="<?= BASE_URL ?>/login">Connexion</a>
     <a class="btn btn-secondary" href="<?= BASE_URL ?>/register">Inscription</a>
 
+    
+    <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item">
+            <a class="pe-3" href="<?= BASE_URL ?>/index" class="">Accueil</a>
+        </li>
+        <li class="nav-item">
+            <a class="pe-3" href="<?= BASE_URL ?>/login" class="">Connexion</a>
+        </li>
+        <li class="nav-item">
+            <a class="pe-3" href="<?= BASE_URL ?>/register" class="">Inscription</a>
+        </li>
+        <li class="nav-item">
+            <a class="pe-3" href="<?= BASE_URL ?>/dashboard" class="">Dashboard</a>
+        </li>
+    </ul>
+    <p class="text-center text-body-secondary">© 2025 Company, Inc</p>
+</footer>
+
     <script src="<?= BASE_URL ?>/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
