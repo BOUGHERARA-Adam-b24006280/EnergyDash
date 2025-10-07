7994983d5a283b018893230a8565dcc32acb6827
Lucaaas_lpp <lepape.lucas@outlook.com>
2025-10-07 20:51:50 +0200

    Ajout d'un favicon

--- views/errors/404.php
+++ views/errors/404.php
@@ -3,6 +3,10 @@
 <head>
   <meta charset="utf-8">
   <title>404</title>
+  <link rel="apple-touch-icon" sizes="180x180" href="<?= BASE_URL ?>/assets/images/favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?= BASE_URL ?>/assets/images/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?= BASE_URL ?>/assets/images/favicon/favicon-16x16.png">
+  <link rel="shortcut icon" href="<?= BASE_URL ?>/assets/images/favicon/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="<?= BASE_URL ?>/assets/bootstrap/css/bootstrap.min.css">
 </head>
 <body class="container py-5">
