21f1af0dc86aa54dd360db159b6eebb9c82e6e73
Lucaaas_lpp <lepape.lucas@outlook.com>
2025-10-07 19:27:28 +0200

    mdp crypté en bcrypt (OAWSP)

--- models/UserModel.php
+++ models/UserModel.php
@@ -8,25 +8,29 @@ class UserModel {
 
         // Vérifier si un email existe déjà
     public function emailExists($email) {
-        $sql = "SELECT COUNT(*) FROM users WHERE email = ?";
+        $sql = "SELECT COUNT(*) FROM users WHERE email = ? LIMIT 1";
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute([$email]);
         return $stmt->fetchColumn() > 0;
     }
         // Enregiste un nouvel utilisateur
     public function register($first_name,$last_name, $email, $password) {
-        $hashedPassword = md5($password);
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
         $sql = "INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?)";
         $stmt = $this->pdo->prepare($sql);
         return $stmt->execute([$first_name, $last_name, $email, $hashedPassword]);
     }
 
     public function login($email, $password) {
-        $hashedPassword = md5($password);
-        $sql = "SELECT * FROM users WHERE email = ? AND password = ?";
+        $sql = "SELECT * FROM users WHERE email = ? LIMIT 1";
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([$email, $hashedPassword]);
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt->execute([$email]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && password_verify($password, $user['password'])) {
+            return $user;
+        }
+        return false;
     }
 }
 ?>
