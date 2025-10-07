f308279edbbbea2f0331b49c78e8ddc558a17143
Lucaaas_lpp <lepape.lucas@outlook.com>
2025-10-07 18:47:10 +0200

    lien href dans navbar mauvais

--- views/shared/navbar.php
+++ views/shared/navbar.php
@@ -5,8 +5,8 @@
     <a class="navbar-brand fw-semibold text-body-secondary" href="<?= BASE_URL ?>/">EnergyDash</a>
 
     <div class="d-flex align-items-center">
-    <a class="btn btn-outline-primary pe-3 ps-3 fw-bold me-3" href="/login">Se connecter</a>
-    <a class="btn btn-primary pe-4 ps-4 fw-bold" href="/register">S'inscrire</a>
+    <a class="btn btn-outline-primary pe-3 ps-3 fw-bold me-3" href="<?= BASE_URL ?>/login">Se connecter</a>
+    <a class="btn btn-primary pe-4 ps-4 fw-bold" href="<?= BASE_URL ?>/register">S'inscrire</a>
     </div>
   </div>
 </nav>  
