3cf682abd5c143b6156c1dae15bf1546289e8ce6
Mohamed <mohamed-amine.haddad@etu.univ-amu.fr>
2025-10-08 20:51:19 +0200

    Premier test de système de MDP oublié

--- config/database.php
+++ config/database.php
@@ -1,13 +1,13 @@
 <?php
-$host = 'localhost';
-$dbname = 'energy_dash';
-$username = 'root'; 
-$password = '';
+    $host = 'mysql-energydash.alwaysdata.net';   // Hôte visible dans ton panneau AlwaysData
+    $dbname = 'energydash_db';                   // nom complet de la base (préfixé par ton ID)
+    $username = '434284';                        // ton identifiant utilisateur MySQL
+    $password = 'HechekUserDeLaBD_69La%$$)Trik'; // le mot de passe MySQL défini dans AlwaysData
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
-}
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die("Erreur de connexion : " . $e->getMessage());
+    }
 ?>
