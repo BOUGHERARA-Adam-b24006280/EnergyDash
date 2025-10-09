bd86cc6bfdb1222abbae9353dd544dbb145cca86
CLOT-GODARD-Kenji-c24022600 <kenjiclotgodard+2@gmail.com>
2025-10-09 12:51:54 +0200

    réglage du dashboard

--- config/routes.php
+++ config/routes.php
@@ -4,7 +4,7 @@ return [
     '' => ['HomeController', 'index'],          // Page d'accueil (/)
     'login' => ['AuthController', 'login'],      // /login
     'register' => ['AuthController', 'register'], // /register*
-    'dashboard' => ['HomeController', 'dashboard'], // /dashboard
+    'dashboard' => ['DashboardController', 'dashboard'], // /dashboard
     'logout' => ['HomeController', 'logout'],
     'mentions-legales' => ['HomeController', 'mentionsLegales'],
     'forgot-password' => ['AuthController', 'forgotPassword'],
---
+++ controllers/DashboardController.php
@@ -0,0 +1,26 @@
+<?php
+class DashboardController {
+    public function dashboard() {
+        
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+        
+        if (!isset($_SESSION['user'])) {
+            header('Location: ' . BASE_URL . '/login');
+            exit;
+        }
+        
+        $title = "Tableau de bord";
+        include __DIR__ . '/../views/dashboard/dashboard.php';
+
+        $title = "Accueil";
+        $body = __DIR__ . '/../views/home/index.php';
+        $navbar = __DIR__ . '/../views/shared/navbar.php';
+        $footer = __DIR__ . '/../views/shared/footer.php';
+        include __DIR__ . '/../views/shared/layout.php';
+
+    }
+
+}
+?>
