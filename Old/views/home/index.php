19005f07605da5888caf0b83d1163af45bba2867
Mohamed <mohamed-amine.haddad@etu.univ-amu.fr>
2025-10-15 14:00:56 +0200

    Suppression de l'ancien projet

--- Old/views/home/index.php
+++
@@ -1,8 +0,0 @@
-
-<main class="d-flex justify-content-center align-items-start" style="min-height:70vh;">
-    <div class="text-center">
-        <h1 class="display-1 fw-bold lh-1">Bienvenue dans<br>votre tableau de bord</h1>
-    </div>
-</main>
-<script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
-
