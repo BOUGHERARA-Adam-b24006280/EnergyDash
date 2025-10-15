695c4dba2c1cc715b80fbc32afea56dec0c1188f
GustinMAILHE <gustin.mailhe@etu.univ-amu.fr>
2025-10-15 17:30:50 +0200

    boutton de theme - en cours

--- IndexTeste.php
+++ IndexTeste.php
@@ -3,3 +3,11 @@
 require_once "controllers/HomeController.php";
 $controller = new HomeController();
 $controller->index();
+
+// Gérer POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action'])) {
+    if ($_POST['action'] === 'switchTheme') {
+        $controller->switchTheme();
+        exit; // Important pour que la redirection fonctionne
+    }
+}
--- views/shared/Header.php
+++ views/shared/Header.php
@@ -1,10 +1,11 @@
 <a href="/">EnergyDash</a>
 
-<div>
 
-    <?php require $_COOKIE['toggleTheme']; ?>
 
-</div>
+<form method="POST" action="IndexTeste.php" style="display: inline;">
+    <input type="hidden" name="action" value="switchTheme">
+    <button type="submit" id="theme-toggle"><?php require $_COOKIE['toggleTheme'];?></button>
+</form>
 
 <div class="d-flex align-items-center">
     <a class="btn btn-outline-primary pe-3 ps-3 fw-bold me-3" href="/login">Se connecter</a>
