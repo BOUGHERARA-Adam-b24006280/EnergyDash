a8404ad102e0a7abe4f1c0848c6f043de4ffac78
GustinMAILHE <gustin.mailhe@etu.univ-amu.fr>
2025-10-15 12:17:05 +0200

    création layout - pas dynamique

--- views/shared/Header.php
+++ views/shared/Header.php
@@ -1,3 +1,6 @@
-<?php
-echo "mon header";
-?>
+
+<a class="nav-link" href="/">EnergyDash</a>
+<div class="d-flex align-items-center">
+    <a class="btn btn-outline-primary pe-3 ps-3 fw-bold me-3" href="/login">Se connecter</a>
+    <a class="btn btn-primary pe-4 ps-4 fw-bold" href="/register">S'inscrire</a>
+</div>
