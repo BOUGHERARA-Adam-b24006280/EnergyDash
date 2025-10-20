7c70818eb50770d2847df7bc245e9a50a65cc719
GustinMAILHE <gustin.mailhe@etu.univ-amu.fr>
2025-10-20 12:00:03 +0200

    import des anciens header et footer

--- src/Views/shared/header.php
+++ src/Views/shared/header.php
@@ -1,4 +1,15 @@
-<header class="mb-4">
-    <h2>EnergyDash</h2>
-    <hr>
+<header>
+    <a href="/">EnergyDash</a>
+
+
+
+    <form method="POST" action="Index.php" style="display: inline;"> <!-- Appel via une requete post -->
+        <input type="hidden" name="action" value="switchTheme"> <!-- défini l'action à "switchTheme" -->
+        <button type="submit" id="theme-toggle"><?php require $_COOKIE['toggleTheme'];?></button> <!-- confirme l'envoie de la requete -->
+    </form>
+
+    <div class="d-flex align-items-center">
+        <a class="btn btn-outline-primary pe-3 ps-3 fw-bold me-3" href="/login">Se connecter</a>
+        <a class="btn btn-primary pe-4 ps-4 fw-bold" href="/register">S'inscrire</a>
+    </div>
 </header>
