cb14efaa64456f8812619fbd1096e2ce45643549
Lucaas_lpp <lepape.lucas@outlook.com>
2025-10-20 14:30:47 +0200

    coleurs + controle de couleurs

--- src/Views/shared/layout.php
+++ src/Views/shared/layout.php
@@ -25,7 +25,7 @@ class Layout {
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title><?= htmlspecialchars(($title ? "$title — EnergyDash" : "EnergyDash")) ?></title>
             <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
-            <link rel="stylesheet" href="/assets/css/style.css">
+            <link rel="stylesheet" href="/assets/css/VarColors.css">
         </head>
         <body class="bg-dark text-light">
             <?php $this->header->render(); ?>
