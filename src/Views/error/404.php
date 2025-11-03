8014002885ac341cbdda91ef4725f55923f79a98
Lucaas_lpp <lepape.lucas@outlook.com>
2025-11-03 14:12:00 +0100

    Code commenté 404 et 500

--- src/Views/error/404.php
+++ src/Views/error/404.php
@@ -1,3 +1,11 @@
+<?php
+/**
+ * Fichier : 404.php
+ * Rôle : Affiche une page d'erreur 404 personnalisée
+ * Auteur : Adam BOUGHERARA, Lucas LEPAPE
+ */
+?>
+
 <h1>Erreur 404</h1>
 <p>La page demandée est introuvable.</p>
 <a href="<?= BASE_URL; ?>">Retour à l’accueil</a>
