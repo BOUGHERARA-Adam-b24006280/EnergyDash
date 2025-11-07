e959f9253fe43ef8c9f91dc512b1fd866d910578
Bougherara Adam <adam.bougherara@etu.univ-amu.fr>
2025-11-07 13:52:19 +0100

    Ajout des erreurs

--- src/Views/auth/forgot.php
+++ src/Views/auth/forgot.php
@@ -1,3 +1,30 @@
+
+<?php if (!empty($errors)): ?>
+    <div class="bg-red-50 border border-red-200 text-sm text-red-800 rounded-lg p-4 dark:bg-red-800/10 dark:border-red-900 dark:text-red-500 mx-2" role="alert" tabindex="-1" aria-labelledby="hs-with-list-label">
+        <div class="flex">
+            <div class="shrink-0">
+                <svg class="shrink-0 size-4 mt-0.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <circle cx="12" cy="12" r="10"></circle>
+                    <path d="m15 9-6 6"></path>
+                    <path d="m9 9 6 6"></path>
+                </svg>
+            </div>
+            <div class="ms-4">
+                <h3 id="hs-with-list-label" class="text-sm font-semibold">
+                    Un problème s'est produit lors de l'envoi de vos données.
+                </h3>
+                <div class="mt-2 text-sm text-red-700 dark:text-red-400">
+                    <ul class="list-disc space-y-1 ps-5">
+                        <?php foreach ($errors as $err): ?>
+                            <li><?= htmlspecialchars($err, ENT_QUOTES, 'UTF-8') ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
+
 <div class="fixed left-0 top-0 -z-10 h-full w-full">
     <div class="absolute inset-0 -z-10 h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] dark:bg-[radial-gradient(#444445_1px,transparent_1px)]  [background-size:16px_16px]"></div>
 </div>
